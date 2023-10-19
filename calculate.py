130aaa0d3265ff56f17c32749e86fa9c77217da2
Ricep29 <ricepardiansyah3@gmail.com>
2023-10-20 00:43:40 +0700

    python-test

---
+++ calculate.py
@@ -0,0 +1,15 @@
+def pertambahan(a, b):
+    result = a, b
+    print("Hasil pertambahan", a, "+", b, "=",result)
+
+def pengurangan(a, b):
+    result = a, b
+    print("Hasil pengurangan", a, "+", b, "=",result)
+
+def perkalian(a, b):
+    result = a, b
+    print("Hasil perkalian", a, "+", b, "=",result)
+
+def pembagian(a, b):
+    result = a, b
+    print("Hasil pembagian", a, "+", b, "=",result)
---
+++ main.py
@@ -0,0 +1,6 @@
+import calculate as c
+
+c.pertambahan(10, 10)
+c.pengurangan(10, 10)
+c.perkalian(10, 10)
+c.pembagian(10, 10)
